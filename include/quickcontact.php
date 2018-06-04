1598e69a01d39811f90ffcbcc710172819359335
zaidhaque <zaidhaque@gmail.com>
2018-06-03 18:03:17 -0700

    Contact test

--- include/quickcontact.php
+++ include/quickcontact.php
@@ -8,8 +8,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'username@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'zaidhaque@gmail.com', // Your Email Address
+				'name' => 'Bespoke' // Your Name
 			);
 
 // Form Processing Messages
